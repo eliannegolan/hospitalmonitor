4e08848d32c17f9e46c279c56d27fc3902f55f14
eliannegolan <elianne.golan19@imperial.ac.uk>
2022-01-07 11:24:34 +0000

    Changes projects

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,17 +1,20 @@
 package com.example.application;
 
+import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.vaadin.artur.helpers.LaunchUtil;
 
 /**
  * The entry point of the Spring Boot application.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.vaadin"})
 @NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
+
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
